6a86d564eafcb859dbfe8a0d9de61520e922ee94
Delaney Kelley <kelleyde@mail.gvsu.edu>
2026-02-12 09:25:00 -0500

    added exit signal

--- write.c
+++ write.c
@@ -5,3 +5,15 @@
 #include <sys/ipc.h>  
 #include <sys/shm.h> 
 #include <sys/ipc.h>
+
+void sigHandler (int sigNum);
+
+
+
+void sigHandler (int sigNum)
+{
+    if(sigNum == SIGINT){
+        printf ("Exit Gracefully\n");
+        exit(0);
+    }
+}
