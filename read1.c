eb9ce273dabc7a56a26dbd6e4faf49aad23f1ec2
Delaney Kelley <kelleyde@mail.gvsu.edu>
2026-02-12 09:33:51 -0500

    add bool test_and_set

--- read1.c
+++ read1.c
@@ -5,3 +5,24 @@
 #include <sys/ipc.h>  
 #include <sys/shm.h> 
 #include <sys/ipc.h>
+#include <stdbool.h>
+
+void sigHandler (int sigNum);
+bool test_and_set(bool *target);
+
+
+
+bool test_and_set(bool *target){
+	bool returnValue = *target;
+	*target = true;
+	return returnValue;
+}
+
+
+void sigHandler (int sigNum)
+{
+    if(sigNum == SIGINT){
+        printf ("Exit Gracefully\n");
+        exit(0);
+    }
+}
