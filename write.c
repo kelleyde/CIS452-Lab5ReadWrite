38301358c688b94a411f9df103e657da894f4100
Delaney Kelley <kelleyde@mail.gvsu.edu>
2026-02-16 18:58:35 -0500

    Added shared mem to write.c. Had to comment out signal handler. Needs testing

--- write.c
+++ write.c
@@ -6,18 +6,43 @@
 #include <sys/shm.h> 
 #include <sys/ipc.h>
 #include <stdbool.h>
+#include <string.h>
+
+#define SIZEOFBLOCK 4096
 
 void sigHandler (int sigNum);
 bool test_and_set(bool *target);
 
-int main() {
-    bool lock = false;
+int main(int argc, char *argv[]) {
+    key_t key = ftok("write.c", 0);
+    int blockId = shmget(key, SIZEOFBLOCK, 0644 | IPC_CREAT);
+    char *blockAddr = shmat(blockId, NULL, 0);
+
+    
+    char inputCheck[100];
+    char input[100];
     do {
+	bool lock = false;
+	printf("Do you want to write to memory, yes or no");
+	fgets(inputCheck, sizeof(inputCheck), stdin);
+	if(inputCheck == "yes"){
+		lock = true;
+	}
+	memcpy(blockAddr, &lock, SIZEOFBLOCK);
         while (test_and_set(&lock)) { } // do nothing
         /* critical section */
+	while(input != "exit"){
+		fgets(input, sizeof(input), stdin);
+		printf("%s", input);
+		strncpy(blockAddr, input, SIZEOFBLOCK);
+	}
         lock = false;
         /* remainder section */
     } while (true);
+
+    shmdt(blockAddr);
+   // signal (SIGINT, sigHandler);
+
 }
 
 bool test_and_set (bool *target)
@@ -27,10 +52,11 @@ bool returnValue = *target;
 return returnValue;
 }
 
-void sigHandler (int sigNum)
-{
-    if(sigNum == SIGINT){
-        printf ("Exit Gracefully\n");
-        exit(0);
-    }
-}
+//void sigHandler (int sigNum)
+//{
+ //   if(sigNum == SIGINT){
+	
+   //     printf ("Exit Gracefully\n");
+    //    exit(0);
+   // }
+//}
