0833c1348423acba6eb4295bb079da2400317588
Delaney Kelley <kelleyde@mail.gvsu.edu>
2026-02-16 19:14:50 -0500

    added ftok and shm

--- read1.c
+++ read1.c
@@ -6,10 +6,13 @@
 #include <sys/shm.h> 
 #include <sys/ipc.h>
 #include <stdbool.h>
+#include <signal.h>
+#define SIZEOFBLOCK 4096
 
 void sigHandler (int sigNum);
 bool test_and_set(bool *target);
-
+//const char *path = "write.c";
+//key_t shm_id = ftok(path, 1);
 
 
 bool test_and_set(bool *target){
@@ -18,16 +21,25 @@ bool test_and_set(bool *target){
 	return returnValue;
 }
 
-bool lock;
 int main() {
+    key_t key = ftok("write.c", 0);
+    int blockId = shmget(key, SIZEOFBLOCK, 0644 | IPC_CREAT);
+    char *blockAddr = shmat(blockId, NULL, 0);
+
 	do {
+		bool lock = true;
+
 		while (test_and_set(&lock));
 		//critical section
+		printf("Read from memory");
+		printf("%s\n", blockAddr);
+
 		lock = false;
 		// remainder
 
 	}while(true);
-
+	shmdt(blockAddr);
+    	signal (SIGINT, sigHandler);
 	return 0; 
 }
 
